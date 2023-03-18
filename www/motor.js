a628ba0ea2121723e36b87b4f6a9d1b374549e4c
jessicaMarqui1 <jessicamarquirocha95@gmail.com>
2023-03-19 07:01:27 +0900

    criei a tabela e so falta retornar valor

--- www/motor.js
+++ www/motor.js
@@ -1,10 +1,28 @@
+const calcularButton = document.querySelector("#calcular");
+const consumoP = document.querySelector("#consumo");
 
-function CalcConsumo(){
-     var km, gas, consumo;
-     km =  document.getElementById('km').value 
-     gas = document.getElementById('gas').value
-     consumo = km / gas
+calcularButton.addEventListener("click", () => {
+  const quilometragem = parseFloat(document.querySelector("#quilometragem").value);
+  const litros = parseFloat(document.querySelector("#litros").value);
 
-     document.querySelector("h2").innerHTML = " Seu consumo foi de " + consumo
+  if (isNaN(quilometragem) || isNaN(litros)) {
+    alert("Por favor, insira valores numéricos.");
+    return;
+  }
 
- }
+  const consumo = quilometragem / litros;
+  consumoP.textContent = `${consumo.toFixed(2)} km/l`;
+
+  const classificacao = document.querySelectorAll("table td");
+  classificacao.forEach((td) => td.classList.remove("destaque"));
+
+  if (consumo > 20) {
+    classificacao[0].classList.add("destaque");
+  } else if (consumo > 12) {
+    classificacao[1].classList.add("destaque");
+  } else if (consumo > 8) {
+    classificacao[2].classList.add("destaque");
+  } else {
+    classificacao[3].classList.add("destaque");
+  }
+});
